2bfe266ee46e4959c82543cb9e0f9b5e730cc3de
sooko <hansmotor89@gmail.com>
2020-04-23 23:04:02 +0700

    add connect wifi example

---
+++ example/connect_wifi.py
@@ -0,0 +1,15 @@
+import network
+def connect():
+    ssid = "yourNetworkName"
+    password =  "yourNetworkPassword"
+    station = network.WLAN(network.STA_IF)
+    if station.isconnected() == True:
+        print("Already connected")
+        return
+    station.active(True)
+    station.connect(ssid, password)
+    while station.isconnected() == False:
+        pass
+    print("Connection successful")
+    print(station.ifconfig())
+connect()
