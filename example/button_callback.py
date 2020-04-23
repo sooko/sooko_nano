dfe9c6fc885b3ccd82ec374a2ee77d593b8a9acb
sooko <hansmotor89@gmail.com>
2020-04-23 22:18:11 +0700

    getting stater

--- example/button_callback.py
+++ example/button_callback.py
@@ -1,1 +1,13 @@
 
+import machine
+from machine import Pin
+
+def callback(pin):
+    if pin.value()==1:
+        print("button pressed")
+    else:
+        print("button_released")
+
+
+button = Pin(0, Pin.IN, Pin.PULL_UP)
+button.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=callback)
