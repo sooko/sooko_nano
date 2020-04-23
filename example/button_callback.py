d4f48601aba44a2ebde77958d117cbe5f4135338
sooko <hansmotor89@gmail.com>
2020-04-23 22:36:05 +0700

    add firmware ditectory

--- example/button_callback.py
+++ example/button_callback.py
@@ -1,13 +1,12 @@
 
 import machine
 from machine import Pin
-
 def callback(pin):
     if pin.value()==1:
         print("button pressed")
     else:
         print("button_released")
-
-
 button = Pin(0, Pin.IN, Pin.PULL_UP)
 button.irq(trigger=Pin.IRQ_RISING | Pin.IRQ_FALLING, handler=callback)
+
+
